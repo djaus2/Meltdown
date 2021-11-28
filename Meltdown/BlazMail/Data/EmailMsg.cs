83232f271d5946cc6c8fcc7441f093534b57e79f
djaus2 <davidjones@sportronics.com.au>
2021-11-28 23:02:06 +1100

    Delimeters to separate file

---
+++ Meltdown/BlazMail/Data/EmailMsg.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using BlazMail.Shared;
+
+using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BlazMail.Data
+{
+
+
+	public class IgnoreEmail
+    {
+		[Key]
+		[Column("Id")]
+		[JsonPropertyName("Id")]
+		public int Id { get; set; }
+
+		[Column("Email")]
+		[JsonPropertyName("Email")]
+		public string Email { get; set; }
+    }
+	public class EmailMsg
+	{
+		private readonly AppSettings Settings;
+		public EmailMsg(AppSettings _settings)
+		{
+			Settings = _settings;
+			clientHost = Settings.ClientHost;
+			fromName = Settings.fromName;
+			fromaEmail = Settings.fromEmail;
+			fromPassword = Settings.fromPassword;
+			string clientPortStr = Settings.ClientPortStr;
+			if (!int.TryParse(clientPortStr, out clientPort))
+			{
+				clientPort = 587;
+			}
+		}
+
+		private string clientHost = "smtp.office365.com";
+		private int clientPort = 587;
+		//private string clientHost = "smtp.gmail.com";
+		//private int clientPort = 587;
+		private string fromName = "<From Name>";
+		private string fromaEmail = "<From Email>";
+		private string fromPassword = "<From Password>";
+
+
+		public async Task<string> SendEmail(EmailUser from, string emailSubject, string emailMessage, List<EmailUser> users)
+		{
+			string response = "OK";
+			Task t;
+			t = Task.Run(() =>
+			{
+				MailMessage msg = new MailMessage();
+
+				msg.From = new MailAddress(from.Email, from.Name); 
+				msg.Subject = emailSubject; 
+				msg.IsBodyHtml = true;
+				msg.Body = $@"<html><body><h1><font color=""red"">{emailSubject}</font></h1>" + emailMessage +"<br/><br/><b>From: <i>Secretary, Athletics Essendon</i></b><br/><i>Nb:Replies go to the Secretary.</i></body></html>";
+				msg.BodyEncoding = System.Text.Encoding.UTF8;
+
+				msg.ReplyToList.Add(from.Email);
+				SmtpClient client = new SmtpClient();
+				client.UseDefaultCredentials = false;
+				client.Credentials = new System.Net.NetworkCredential(from.Email, from.Password);
+				client.Port = clientPort; 
+				client.Host = clientHost; 
+				client.DeliveryMethod = SmtpDeliveryMethod.Network;
+				client.EnableSsl = true;
+				if (users != null)
+				{
+					foreach (var usr in users)
+					{
+						msg.Bcc.Add(new MailAddress(usr.Email, usr.Name));
+					}
+				}
+				int count = msg.Bcc.Count;
+				try
+				{
+					client.Send(msg);
+					Console.WriteLine("Email/s Successfully Sent");
+					System.Diagnostics.Debug.WriteLine($"{count} Emails Successfully Sent by: {fromName}");
+					response = $"{count} Emails Successfully Sent From: {fromName}";
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+					System.Diagnostics.Debug.WriteLine(ex.Message);
+					response = $"Error: {ex.Message}";
+				}
+			}
+			);
+			await t;
+			return response;
+		}
+	}
+}
--- Meltdown/Meltdown/Meltdown.cs
+++ Meltdown/Meltdown/Meltdown.cs
@@ -6,61 +6,8 @@ namespace Meltdown
     /// <summary>
     /// This is a partial implementation of the the second schema, with some other extras.
     /// </summary>
-    public static class Meltdown
+    public static partial class Meltdown
     {
-        /// <summary>
-        /// Default delimeters.
-        /// Note only supply the left hand delimeter. Right hand is generated from it.
-        /// </summary>
-        private static string bold = "[[";
-        private static string italics = "((";
-        private static string underline = "{{";
-
-        // Combinations use a juxtaposition of the second characters of the 3 above
-        private static string bold_italics = $"{bold[1]}{italics[1]}";
-        private static string bold_underline = $"{bold[1]}{underline[1]}";
-        private static string italics_underline = $"{italics[1]}{underline[1]}";
-        private static string bold_italics_underline = $"{bold[1]}{italics[1]}{underline[1]}";
-
-        private static string ccolor = "(((";
-        private static string llink = "{{{";
-        private static char paramSep = '|';
-
-        // Next 3 are used for searches, formed from previous 3
-        // Searches use System.IO.Enumeration.FileSystemName.MatchesSimpleExpression()
-        private static string ColorPattern = $"*{ccolor}*{paramSep}*{Reverse(ccolor)}*";
-        private static string WebPatternwithText = $"*{llink}*{paramSep}*{Reverse(llink)}*";
-        private static string WebPatternwithoutText = $"*{llink}*{Reverse(llink)}*";
-        private static string HeadingPatternatStartofLine = $"[[?]]*";
-        private static string ListPatternatStartofLine = $"((?))*";
-
-        /// <summary>
-        /// Change the default delimeters by sending 1 or 2 Csv lists
-        /// </summary>
-        /// <param name="formatCsv">CSV list of bold,italic,underline strings</param>
-        /// <param name="webColorCsv">Csv list of font color and link deleimeters plus arg delimeter</param>
-        static void SetDelimters(string formatCsv, string webColorCsv = "(((,{{{,|")
-        {
-            if (string.IsNullOrEmpty(formatCsv))
-                return;
-            string[] delims = formatCsv.Split(',');
-            if (delims.Length < 3)
-                return;
-            bold = delims[0];
-            italics = delims[1];
-            underline = delims[2];
-            if (string.IsNullOrEmpty(webColorCsv))
-                return;
-            delims = webColorCsv.Split(',');
-            if (delims.Length < 3)
-                return;
-            ccolor = delims[0];
-            llink = delims[1];
-            string strn = delims[2];
-            if (strn.Length == 1)
-                paramSep = strn[0]; ;
-        }
-
         // This is a subset of permitted HTML colors
         private static List<string> colors = new List<string>
         {
@@ -85,16 +32,6 @@ namespace Meltdown
             "White",
             "Yellow"
         };
-
-        //Ref: https://stackoverflow.com/questions/228038/best-way-to-reverse-a-string
-        public static string Reverse(string s)
-        {
-            char[] charArray = s.ToCharArray();
-            Array.Reverse(charArray);
-            string ret = new string(charArray);
-            ret = ret.Replace('[', ']').Replace('(', ')').Replace('{', '}');//.Replace('<','>');
-            return ret;
-        }
 
         public static string Parse(string txt)
         {
